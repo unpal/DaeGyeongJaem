702ee3fc9669c2d226e8655cb91c24675c796b88
dotori <dotori_muuk@outlook.kr>
2026-07-16 17:31:54 +0900

    PortalManager.cs 추가

---
+++ Assets/Script/dotori/PortalManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 특정 개수의 오브젝트(포탈)을 활성화시키는 코드입니다
+ * 게임매니저 등의 외부에서 호출해야 합니다
+ */
+public class PortalManager : MonoBehaviour
+{
+    public List<GameObject> portals = new List<GameObject>();
+
+    public void ActivateRandomPortals(int n)
+    {
+        if (portals == null || portals.Count == 0) return;
+
+        List<GameObject> availablePortals = new List<GameObject>();
+        foreach (var portal in portals)
+        {
+            if (portal != null)
+                availablePortals.Add(portal);
+        }
+
+        int count = Mathf.Min(n, availablePortals.Count);
+        for (int i = 0; i < count; i++)
+        {
+            int randomIndex = Random.Range(0, availablePortals.Count);
+            availablePortals[randomIndex].SetActive(true);
+            availablePortals.RemoveAt(randomIndex);
+        }
+    }
+}
